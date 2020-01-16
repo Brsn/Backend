dcb20a484ad3e0c2faad290a28b9b5d79d85d69b
Bronson Brown <Brsn90@icloud.com>
2020-01-16 13:21:06 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -5,15 +5,24 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const todoRoutes = express.Router();
 const PORT = 5000;
-
+const path = require('path');
 let Todo = require('./todo.model');
 
 //middleware
 app.use(cors());
+app.all('*', function (req, res, next) {
+    var origin = req.get('origin');
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+//mongoose.connect('mongodb+srv+https://admin:MJC3vhvKXTaWEpUv@helioatlas-1vrvg.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
 //connect mongoose with the local mongo server
+mongoose.connect('mongodb+srv://admin:MJC3vhvKXTaWEpUv@helioatlas-1vrvg.mongodb.net/test', { useNewUrlParser: true })
 const connection = mongoose.connection;
 
 connection.once('open', function () {
@@ -90,6 +99,13 @@ todoRoutes.route('/update/:id').post(function (req, res) {
 app.use('/todos', todoRoutes);
 //uses express routes
 
+/* if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'build', 'index'));
+    })
+} */
 app.listen(PORT, function () {
     console.log("Server is running on Port: " + PORT);
 })
