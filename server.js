3ef440a3db0e3b14d04f32a1e39c5d63e58873b2
Bronson Brown <Brsn90@icloud.com>
2020-03-25 07:18:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ todoRoutes.route('/').post(function (req, res) {
     let todo = new Todo(req.body);
     todo.save()
         .then(todo => {
-            res.status(200).json({ 'todo': req.params.id + req.body });
+            res.status(200).json({ 'todo': todo });
         })
         .catch(err => {
             res.status(400).send('adding new todo failed');
